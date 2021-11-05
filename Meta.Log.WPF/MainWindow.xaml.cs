f39ad08acf0d0c075de5dd61c42bfe6bdf2ef1d4
bakanov-g <bakanov-g@speechpro.com>
2021-11-05 14:14:00 +0300

    wpf project changes

--- Meta.Log.WPF/MainWindow.xaml.cs
+++ Meta.Log.WPF/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Meta.Log.WPF
 {
@@ -20,9 +8,33 @@ namespace Meta.Log.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        int count = 0;
+
+        //public static void OnCloseWindow(object sender, ExecutedRoutedEventArgs e)
+        //{
+        //    if (e.Parameter is MainWindow window)
+        //        window.Close();
+        //}
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+            CounterLabel.Content = $"Current count: {count}";
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
